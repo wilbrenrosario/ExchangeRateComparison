75eaef5c7c8e35c4fe4a81902e8eb83f77570158
Wilbren Ambiorix Rosario Serrano <wrosario@argentuminc.com>
2025-08-03 08:53:45 -0400

    Actualizacion corrida local

--- ExchangeRateComparison.Api/Program.cs
+++ ExchangeRateComparison.Api/Program.cs
@@ -19,19 +19,29 @@ namespace ExchangeRateComparison.Api
 
             builder.Services.AddScoped<ExchangeRateService>();
 
+            // Detectar si está ejecutándose en un contenedor
+            bool isRunningInContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+            // Configurar las URLs base según el entorno
+            string api1BaseUrl = isRunningInContainer ? "http://api1:81" : "http://localhost:81";
+            string api2BaseUrl = isRunningInContainer ? "http://api2:82" : "http://localhost:82";
+            string api3BaseUrl = isRunningInContainer ? "http://api3:83" : "http://localhost:83";
+
             builder.Services.AddHttpClient("Provider1", client =>
             {
-                client.BaseAddress = new Uri("http://api1:81");
+                client.BaseAddress = new Uri(api1BaseUrl);
                 client.DefaultRequestHeaders.Add("Authorization", "Bearer token-api1");
             });
+
             builder.Services.AddHttpClient("Provider2", client =>
             {
-                client.BaseAddress = new Uri("http://api2:82");
+                client.BaseAddress = new Uri(api2BaseUrl);
                 client.DefaultRequestHeaders.Add("Authorization", "Bearer token-api2");
             });
+
             builder.Services.AddHttpClient("Provider3", client =>
             {
-                client.BaseAddress = new Uri("http://api3:83");
+                client.BaseAddress = new Uri(api3BaseUrl);
                 client.DefaultRequestHeaders.Add("Authorization", "Bearer token-api3");
             });
 
